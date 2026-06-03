16144040bb59d441607807792693e3d590209920
Tristan Cravello <tlcravello@gmail.com>
2026-06-02 21:12:40 -0400

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        const map = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "[",
+        }
+
+        for (let i = 0; i < s.length; i++) {
+            let char = s[i];
+
+            if (char === "{" || char === "(" || char === "[") {
+                stack.push(char);
+            } else {
+                if (stack[stack.length - 1] !== map[char]) return false;
+                stack.pop();
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
